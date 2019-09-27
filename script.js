8aa5a65298a7be06d8f942055c9abfb454f0cb58
Pascal <fedodev55@gmail.com>
2019-09-27 13:38:11 +0200

    Ouverture menu responsive

---
+++ script.js
@@ -0,0 +1,15 @@
+// Move menu
+const clicHamburger = document.getElementById('clickHamburger');
+const moveMenu = document.getElementById('moveMenu');
+clicHamburger.addEventListener('click', () => {
+    moveMenu.style.transition = "0.5s";
+    moveMenu.style.left = "0";
+
+    
+});
+//About
+const clickAbout = document.getElementById('about');
+clickAbout.addEventListener('click', () => {
+    alert('aa');
+
+});
