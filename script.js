57c9efb948a3546fecadf57a6d5982e588fc1a8c
Pascal <fedodev55@gmail.com>
2019-09-30 20:26:10 +0200

    mise en place About

--- script.js
+++ script.js
@@ -4,12 +4,81 @@ const moveMenu = document.getElementById('moveMenu');
 clicHamburger.addEventListener('click', () => {
     moveMenu.style.transition = "0.5s";
     moveMenu.style.left = "0";
-
-    
 });
-// About
-const clickAbout = document.getElementById('about');
+
+//close choose menu
+function paramCloseMenu(){
+    moveMenu.style.left = "-400px";
+    moveMenu.style.transition = "0.5s";
+};
+const clickCloseMenuGesture = document.getElementById('menuGesture');
+const clickCloseMenucontact = document.getElementById('menucontact');
+const clickCloseMenuSuggestion = document.getElementById('menuSuggestion');
+const clickCloseMenuAbout = document.getElementById('menuAbout');
+clickCloseMenuGesture.addEventListener('click', () => { //close menu gesture
+    paramCloseMenu();
+});
+clickCloseMenucontact.addEventListener('click', () => { //close menu contact
+    paramCloseMenu();
+});
+clickCloseMenuSuggestion.addEventListener('click', () => { //close menu suggestion
+    paramCloseMenu();
+});
+clickCloseMenuAbout.addEventListener('click', () => { //close menu about
+    paramCloseMenu();
+});
+
+// Open about
+const clickAbout = document.getElementById('menuAbout');
+const openAbout = document.getElementById('aboutOf');
+
+
 clickAbout.addEventListener('click', () => {
-    alert('aa');
+    openAbout.style.display = "flex";
+    openAbout.style.zIndex = "3000";
 
 });
+
+//Assess
+const moveQuestionAssess = document.getElementById('assess');
+const clickAssess = document.getElementById('assessBtn');
+const colorHeaderMask = document.getElementById('headerMask');
+
+clickAssess.addEventListener('click', () => {
+    document.body.style.background = 'rgba(1,1,1,0.3)'.replace(/[^,]+(?=\))/, '0.5');
+    colorHeaderMask.style.fill = '#7D7C7C';
+    moveQuestionAssess.style.zIndex = "999";
+    moveQuestionAssess.style.opacity = "1";
+    moveQuestionAssess.style.transition = '2s';
+
+});
+
+//Close modal
+window.onclick = function () {
+    setTimeout(function () {
+        if (openAbout.style.zIndex == 3000) {
+            openAbout.style.zIndex = "-2";
+            openAbout.style.display = "none";
+        }
+    }, 3000)
+};
+
+
+
+
+function hamburger_click(open)
+{
+    // var hamburgerElement = document.getElementById("hamburger");
+    // var menuElement = document.getElementById("menu");
+
+    if (open) {
+        // hamburgerElement.style.display = "none";
+        moveMenu.style.transition = "0.5s";
+        moveMenu.style.left = "-400";
+    } else {
+        // hamburgerElement.style.display = "block";
+        // moveMenu.style.display = "none";
+        // moveMenu.style.transition = "0.5s";
+        moveMenu.style.left = "-400px";
+    }
+};
